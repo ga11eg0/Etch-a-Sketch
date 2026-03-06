92fe606bf37d38068311974fb4b7fdda66c1215f
ga11ego <og61255@gmail.com>
2026-03-06 11:59:10 -0500

    grid is done

--- main.js
+++ main.js
@@ -3,14 +3,15 @@
 const board = document.querySelector("#board");
 
 
-
+let w = 100/16
+let h = 600/16
 for ( let i = 0; i < 16*16; i++ ){
     // 2. create an element 
     const square = document.createElement("sq")
     // adds ti to a class 
     square.classList.add("cell")
     //add style
-    square.style.cssText = "width: 16px; height: 16px; border: 2px solid gray; "
+    square.style.cssText = `width: ${w}%; height: ${h}px; border: 2px solid gray; box-sizing:border-box; flex-shrink: 1;`
     // 3. add new element to selected element 
     board.appendChild(square)
 }
